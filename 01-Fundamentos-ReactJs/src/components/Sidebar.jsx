682233b5d9ef89e8a5b2a0662019d496b17ac872
Wellington Carvalho <well.f.carvalho@gmail.com>
2025-01-30 01:16:02 -0300

    Aula inicial para criacao do layout da pagina de feed

---
+++ 01-Fundamentos-ReactJs/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilLine} from 'phosphor-react'
+import styles from './Sidebar.module.css'
+
+export function Sidebar(){
+    return(
+        <asside className={styles.sidebar}>
+            <img 
+            className={styles.cover}
+            src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/wellingtonC.png" />
+                <strong>Wellington Carvalho</strong>
+                <span>Fullstack Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar Perfil
+                </a>
+            </footer>
+        </asside>
+    );
+}
