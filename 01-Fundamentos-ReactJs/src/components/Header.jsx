90659c04be061bfb31dab7cfc866104c7a1b9df1
wellington carvalho <well.f.carvalho@gmail.com>
2025-01-31 11:25:40 -0300

    feat(EStudo React): :art: adicionando title

--- 01-Fundamentos-ReactJs/src/components/Header.jsx
+++ 01-Fundamentos-ReactJs/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import styles from './Header.module.css'
-import igniteLogo from'../assets/Ignite-logo.svg'
-export function Header()
-{
+import igniteLogo from '../assets/Ignite-logo.svg'
+export function Header() {
     return (
         <header className={styles.header}>
-            <img src={igniteLogo} alt="Logotipo Ignite"/>
+            <img src={igniteLogo} alt="Logotipo Ignite" />
+            <h1>Ignite Feed</h1>
         </header>
-        
+
     );
 
 }
